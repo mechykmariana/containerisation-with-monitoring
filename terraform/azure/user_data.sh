c71794fdfc549ee8f70d6f29282e7ec679b2f095
mechykmariana <maryanamechik3@gmail.com>
2025-04-08 19:25:43 +0000

    userdata added

---
+++ terraform/azure/user_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+exec > /var/log/user-data.log 2>&1
+set -x
+apt update -y
+apt install -y docker.io docker-compose
+systemctl start docker
+systemctl enable docker
+cd /home/azureuser/app
+docker-compose up -d
